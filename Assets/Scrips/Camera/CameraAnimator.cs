1583d1100f176f8732f91baf408db4c34a7c5282
gonzaloiv <gonzalo.iglesias@teimas.com>
2017-02-10 19:04:44 +0100

    Separar el funcionamiento de las animaciones del funcionamiento de los estados en la camara

---
+++ Assets/Scrips/Camera/CameraAnimator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimator : MonoBehaviour {
+
+  #region Fields
+
+  private CameraController cameraController;
+  private GameObject player;
+
+  private float maxSpeed = Config.CameraMaxSpeed;
+  private Vector3 velocity = Vector3.zero;
+  private int distanceToPlayer = Config.CameraDistanceToPlayer;
+
+
+  #endregion
+
+  #region Mono Behaviour
+
+  void Update() {
+
+    // Credits: http://answers.unity3d.com/questions/824950/rotate-a-gameobject-to-player-position.html
+    Vector3 direction = player.transform.position - transform.position;
+    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    Quaternion qto = Quaternion.AngleAxis(angle, Vector3.forward);
+
+    Vector3 playerPosition;
+    Quaternion qto2;
+
+    switch(cameraController.CameraState) {
+      case CameraState.Left:
+        playerPosition = new Vector3(player.transform.position.x + 30, transform.position.y, player.transform.position.z);
+        qto2 = Quaternion.Euler(0, qto.eulerAngles.y - 90 , 0);
+        break;
+      case CameraState.Right:
+        playerPosition = new Vector3(player.transform.position.x - 30, transform.position.y, player.transform.position.z);
+        qto2 = Quaternion.Euler(0, qto.eulerAngles.y + 90 , 0);
+        break;
+        break;
+      default:
+        playerPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z - 30);
+        qto2 = Quaternion.Euler(qto.eulerAngles.x, qto.eulerAngles.y, 0);
+        break;
+    }
+
+    transform.rotation = Quaternion.Slerp(transform.rotation, qto2, 10f * Time.deltaTime);       
+    transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref velocity, maxSpeed);
+
+  }
+
+  #endregion
+
+  #region Public Behaviour
+
+  public void Initialize(CameraController cameraController, GameObject player) {
+    this.cameraController = cameraController;
+    this.player = player;
+  }
+
+  #endregion
+	
+}
--- Assets/Scrips/Camera/CameraController.cs
+++ Assets/Scrips/Camera/CameraController.cs
@@ -2,55 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : StateMachine {
 
   #region Fields
 
+  public GameObject Player { get { return player; } }
   private GameObject player;
+  
+  private CameraAnimator cameraAnimator;
 
-  private float maxSpeed = Config.CameraMaxSpeed;
-  private Vector3 velocity = Vector3.zero;
-  private int distanceToPlayer = Config.CameraDistanceToPlayer;
+  public CameraState CameraState { get { return cameraState; } }
   private CameraState cameraState = CameraState.Straight;
-
+ 
   #endregion
 
   #region Mono Behaviour
 
+  void Awake() {
+    cameraAnimator = GetComponent<CameraAnimator>();
+  }
+
   void OnEnable() {
     EventManager.StartListening<TurnLeftEvent>(OnTurnLeftEvent);
     EventManager.StartListening<TurnRightEvent>(OnTurnRightEvent);
-  }
-
-  void Update() {
-
-    // Credits: http://answers.unity3d.com/questions/824950/rotate-a-gameobject-to-player-position.html
-    Vector3 direction = player.transform.position - transform.position;
-    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-    Quaternion qto = Quaternion.AngleAxis(angle, Vector3.forward);
-
-    Vector3 playerPosition;
-    Quaternion qto2;
-
-    switch(cameraState) {
-      case CameraState.Left:
-        playerPosition = new Vector3(player.transform.position.x + 30, transform.position.y, player.transform.position.z);
-        qto2 = Quaternion.Euler(0, qto.eulerAngles.y - 90 , 0);
-        break;
-      case CameraState.Right:
-        playerPosition = new Vector3(player.transform.position.x - 30, transform.position.y, player.transform.position.z);
-        qto2 = Quaternion.Euler(0, qto.eulerAngles.y + 90 , 0);
-        break;
-        break;
-      default:
-        playerPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z - 30);
-        qto2 = Quaternion.Euler(qto.eulerAngles.x, qto.eulerAngles.y, 0);
-        break;
-    }
-
-    transform.rotation = Quaternion.Slerp(transform.rotation, qto2, 10f * Time.deltaTime);       
-    transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref velocity, maxSpeed);
-
   }
 
   void OnDisable() {
@@ -75,7 +49,7 @@ public class CameraController : MonoBehaviour {
   #region Public Behaviour
   
   public void Initialize(GameObject player) {
-    this.player = player;
+    cameraAnimator.Initialize(this, player);
   }
   
   #endregion
